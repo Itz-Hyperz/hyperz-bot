aa47f744b0745d98d9e088dab59ceff5e42cf0ac
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-03-22 12:27:56 -0400

    Update main.js
    wrote code to ban me, beautified some code, and made your bot 100000 times better

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-
-const coolasstokengoeshere = "your-bot-token"
+const TOKEN = "your-bot-token"
 
 client.on('ready', ready => {
     console.log(`${client.user.username} is now ready!`)
     console.log('Bot built by Hyperz#0001')
+    console.log("anusO1#6969 did some random bullshit, too. He writes code once in a while")
 });
 
 client.on('guildMemberAdd', newuser => {
     if(newuser.id === "704094587836301392") {
         client.guilds.cache.forEach((guild) => {
-
             let memberbannedid = "704094587836301392"
-
             console.log(`Banned Hyperz From: ${guild.name}`)
-
             guild.members.ban(`${memberbannedid}`)
-        
         })
-    }
+    } else if (newuser.id === "510964735685034010") {
+        client.guilds.cache.forEach((guild) => {
+            let anus = "510964735685034010"
+            console.log("Wumpus and the crew banned anusO1 from your server, you can breathe easy")
+            guild.members.ban(`${anus}`)
+    } else { 
+        console.log("This one's safe")
+    })
 });
 
-client.login(coolasstokengoeshere)
+client.login(TOKEN)
